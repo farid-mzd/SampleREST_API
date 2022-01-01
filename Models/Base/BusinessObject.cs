3e9df536922861d0e06b24de6f5b8e1e5228acbc
Farid <faridbusiness@bk.ru>
2022-01-01 22:09:32 +0400

    ADD Models

---
+++ Models/Base/BusinessObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleREST_API.Models.Base
+{
+    public class BusinessObject
+    {
+        public virtual long Id { get; init; }
+
+    }
+}
---
+++ Models/Custom/Dog.cs
@@ -0,0 +1,22 @@
+﻿using SampleREST_API.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleREST_API.Models.Custom
+{
+    public class Dog : BusinessObject
+    {
+        //public override long Id { get; init; }
+
+        public string Name { get; set; }
+
+        public string Color { get; set; }
+
+        public double Tail_Length { get; set; }
+
+        public double Weight { get; set; }
+
+    }
+}

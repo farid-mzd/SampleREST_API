d816be9a9586478eeb98b7f994b8bce6c175782e
Farid <faridbusiness@bk.ru>
2022-01-02 23:35:09 +0400

    ADDED Abstract service

---
+++ Services/Abstract/IDogService.cs
@@ -0,0 +1,15 @@
+﻿using SampleREST_API.Models.Custom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleREST_API.Services.Abstract
+{
+    public interface IDogService
+    {
+        IEnumerable<Dog> GetDogs();
+
+        Dog AddDog(Dog dog);
+    }
+}
---
+++ Services/Concrete/DogService.cs
@@ -0,0 +1,22 @@
+﻿using SampleREST_API.Models.Custom;
+using SampleREST_API.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleREST_API.Services.Concrete
+{
+    public class DogService : IDogService
+    {
+        public Dog AddDog(Dog dog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Dog> GetDogs()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

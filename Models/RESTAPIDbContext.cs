d3d97b7339139a463ef5f151407ecf08268bc131
Farid <faridbusiness@bk.ru>
2022-01-03 00:50:50 +0400

    ADDED DbContext

--- Models/RESTAPIDbContext.cs
+++ Models/RESTAPIDbContext.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SampleREST_API.Models.Custom;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SampleREST_API.Models
 {
-    public class RESTAPIDbContext 
+    public class RESTAPIDbContext : DbContext
     {
+        public DbSet<Dog> Dogs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-O2A200U\GHOSTWALKER;Initial Catalog=REST_API_DB; Integrated Security=True");
+
+            base.OnConfiguring(optionsBuilder);
+        }
     }
+
 }

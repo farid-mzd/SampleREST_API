7c416d0dc458836b6cc0c147f5f8af909387a92e
Farid <faridbusiness@bk.ru>
2022-01-03 00:29:16 +0400

    ADDED EF core dependencies

---
+++ Models/RESTAPIDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleREST_API.Models
+{
+    public class RESTAPIDbContext 
+    {
+    }
+}

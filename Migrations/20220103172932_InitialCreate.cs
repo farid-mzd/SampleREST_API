4c12ba1028ab790d9d0501931d83570911b3f01e
Farid <faridbusiness@bk.ru>
2022-01-03 21:30:24 +0400

    ADDED Migration

---
+++ Migrations/20220103172932_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SampleREST_API.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dogs",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Tail_Length = table.Column<double>(type: "float", nullable: false),
+                    Weight = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dogs", x => x.Name);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Dogs");
+        }
+    }
+}
